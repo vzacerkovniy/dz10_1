24747319e65551e45af3fe067e2903fe6421e7d8
vlad <vlad_26reg@mail.ru>
2024-07-01 11:09:29 +0300

    правки в функциях модуля widget

--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,18 @@
 def mask_account_card(number: str) ->str:
-    if "Счет" in number:
-        mask_number = number[:-20] + "**" + number[-4:]
-        return mask_number
+    if number != "":
+        if "Счет" in number:
+            mask_number = number[:-20] + "**" + number[-4:]
+            return mask_number
+        else:
+            mask_number = number[:-16] + number[-16:-12] + " " + number[-12:-10] + "** **** " + number[-4:]
+            return mask_number
     else:
-        mask_number = number[:-16] + number[-16:-12] + " " + number[-12:-10] + "** **** " + number[-4:]
-        return mask_number
+        return "Неверный ввод"
 
 
 def get_data(data: str) ->str:
-    new_data = data[8:10] + "." + data[5:7] + "." + data[0:4]
-    return new_data
+    if data != "":
+        new_data = data[8:10] + "." + data[5:7] + "." + data[0:4]
+        return new_data
+    else:
+        return "Неверный ввод"
