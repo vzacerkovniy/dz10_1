041efd03ae1b0c1a7cedc9ff9e7e055afa530a38
v.zacerkovniy <vlad_26reg@mail.com>
2024-07-08 16:03:33 +0300

    changed filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,16 @@
-def filter_by_state(input_list: list, state: str = "EXECUTED") -> str:
+def filter_by_state(input_list: list, state: str = "EXECUTED") -> list:
     """Функция фильтрации списка словарей по значению ключа"""
     executed = []
     canceled = []
     for operation in input_list:
-        if operation["state"] != state:
+        if operation["state"] == "CANCELED":
             canceled.append(operation)
         else:
             executed.append(operation)
-    return f"{executed}\n{canceled}"
+    if state == "CANCELED":
+        return canceled
+    else:
+        return executed
 
 
 def sort_by_date(input_list: list) -> list:
