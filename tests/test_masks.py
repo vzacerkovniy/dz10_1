d2d5e5bb56e529b40b6de24d040237c911d27dad
v.zacerkovniy <vlad_26reg@mail.com>
2024-07-17 11:23:57 +0300

    update processing, tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,35 +1,66 @@
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_get_mask_card_number() -> None:
-    assert get_mask_card_number("Карта 1111 1111 1111 1111") == "Карта 1111 11** **** 1111"
-
-    assert get_mask_card_number("Карта1111111111111111") == "Карта 1111 11** **** 1111"
-
-    assert get_mask_card_number("Visa 1111 1111 1111 1111") == "Visa 1111 11** **** 1111"
-
-    assert get_mask_card_number("1111 1111 1111 1111") == "Неверный ввод"
-
-    assert get_mask_card_number("") == "Неверный ввод"
-
-    assert get_mask_card_number("Carl Jonson") == "Неверный ввод"
-
-    assert get_mask_card_number("Карта 1111 1111 1111 1111 1111") == "Неверный ввод"
-
-    assert get_mask_card_number("Карта 1111 1111 1111") == "Неверный ввод"
+# Тесты с использованием фикстур
 
 
-def test_get_mask_account() -> None:
-    assert get_mask_account("Счет 11111111111111111111") == "Счет **1111"
+# Для get_mask_card_number
+def test_get_mask_card_number(num_usual: str) -> None:
+    assert get_mask_card_number("Карта 1111 1111 1111 1111") == num_usual
 
-    assert get_mask_account("Счет11111111111111111111") == "Счет **1111"
 
-    assert get_mask_account("Peppa") == "Неверный ввод"
+def test_get_mask_card_number_1(num_usual: str) -> None:
+    assert get_mask_card_number("Карта1111111111111111") == num_usual
 
-    assert get_mask_account("11111111111111111111") == "Неверный ввод"
 
-    assert get_mask_account("Счет 111") == "Неверный ввод"
+def test_get_mask_card_number_2(num_visa: str) -> None:
+    assert get_mask_card_number("Visa 1111 1111 1111 1111") == num_visa
 
-    assert get_mask_account("Счет 123456789123456789123456789") == "Неверный ввод"
 
-    assert get_mask_account("") == "Неверный ввод"
+def test_get_mask_card_number_3(num_invalid: str) -> None:
+    assert get_mask_card_number("1111 1111 1111 1111") == num_invalid
+
+
+def test_get_mask_card_number_4(num_invalid: str) -> None:
+    assert get_mask_card_number("Карта 1111 1111 1111") == num_invalid
+
+
+def test_get_mask_card_number_5(num_invalid: str) -> None:
+    assert get_mask_card_number("Карта 1111 1111 1111 1111 1111") == num_invalid
+
+
+def test_get_mask_card_number_6(num_invalid: str) -> None:
+    assert get_mask_card_number("") == num_invalid
+
+
+def test_get_mask_card_number_7(num_invalid: str) -> None:
+    assert get_mask_card_number("Carl Jonson") == num_invalid
+
+
+# Для get_mask_account
+def test_get_mask_account(account_usual: str) -> None:
+    assert get_mask_account("Счет 11111111111111111111") == account_usual
+
+
+def test_get_mask_account_1(account_usual: str) -> None:
+    assert get_mask_account("Счет11111111111111111111") == account_usual
+
+
+def test_get_mask_account_2(account_invalid: str) -> None:
+    assert get_mask_account("Peppa") == account_invalid
+
+
+def test_get_mask_account_3(account_invalid: str) -> None:
+    assert get_mask_account("11111111111111111111") == account_invalid
+
+
+def test_get_mask_account_4(account_invalid: str) -> None:
+    assert get_mask_account("Счет 111") == account_invalid
+
+
+def test_get_mask_account_5(account_invalid: str) -> None:
+    assert get_mask_account("Счет 123456789123456789123456789") == account_invalid
+
+
+def test_get_mask_account_6(account_invalid: str) -> None:
+    assert get_mask_account("") == account_invalid
